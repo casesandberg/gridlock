9d8e10b0224d27c2b808c4ad2565f128065d46c6
Adrik <adrik@iterit.com>
2014-07-26 14:45:24 -0700

    added collections

---
+++ gridlock/collections.js
@@ -0,0 +1,27 @@
+//Data Definitions
+/*
+Intersection: document             intersection in the grid
+  roads: object                   object of the roads eminating from this intersection
+    ne: object                    road direction
+      queue: [carId]              ordered list of cars
+      interId: UUID               intersection this road connects to
+      type: string                human/empty/computer
+    nw...
+    se...
+    sw...
+
+Car: document
+  direction: string               left/straight/right
+  history: [UUID]                 intersections car has passed
+  skin: int                       look of car
+
+User: document
+  score: int                      number of cars passed
+  interId: UUID                   assigned intersection
+  name: string                    name
+  avatar: string                  href to photo from FB
+*/
+//User collection created by accounts package
+
+Intersections = new Meteor.Collection("intersections");
+Cars = new Meteor.Collection("cars");
