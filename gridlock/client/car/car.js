367b98b31ae4028114584e0e1e18975c8e5a5da4
casesandberg <case@casesandberg.com>
2014-07-27 08:25:34 -0700

    Direction hard-coded

--- gridlock/client/car/car.js
+++ gridlock/client/car/car.js
@@ -2,5 +2,9 @@ Template.car.rendered = function() {
 	//console.log(this.data);
 	var car = Cars.findOne(this.data);
 	console.log(car)
-	// $('#'+this.data).html(JSON.stringify(car))
+	// $('#'+this.data).html(JSON.stringify(car));
+
+	if(car.current == 'NW' && car.direction == 'NE' || car.current == 'NE' && car.direction == 'SE' || car.current == 'SE' && car.direction == 'SW' || car.current == 'SW' && car.direction == 'NW'){
+		car.direction = '&lt;';
+	}
 }
