6b6971d2e118b110f366c02769ef60e0397f86ac
Chris Gervang <gervangster@live.com>
2014-07-27 10:46:27 -0700

    hud congestion

--- gridlock/client/car/car.js
+++ gridlock/client/car/car.js
@@ -1,42 +1,41 @@
 Template.car.helpers({
     carName: function() {
-    var carId = this.toString();
-    var quadrant;
-    var car = Cars.findOne({_id: carId});
+      var carId = this.toString();
+      var quadrant;
+      var car = Cars.findOne({_id: carId});
 
-    var intersection = Intersections.findOne({
-        $or: [
-            {"roads.ne.queue": carId}, 
-            {"roads.nw.queue": carId}, 
-            {"roads.sw.queue": carId}, 
-            {"roads.se.queue": carId}
-        ]
-    });
-    var id = intersection._id;
-    _.forEach(intersection.roads, function(val, key) {
-        //console.log("key: ", key, "val: ", val)
-        for (var i = 0; i < val.queue.length; i++) {
-            if(val.queue[i] === carId) {
-                //found the car in road!!!
-                // console.log("Found: ", val.queue[i], "in road: ", key);
-                quadrant = key;
-                car.current = quadrant;
-            }
-        };
-    });
-    if(car.current == 'nw' && car.direction == 'ne' || car.current == 'ne' && car.direction == 'se' || car.current == 'se' && car.direction == 'sw' || car.current == 'sw' && car.direction == 'nw'){
-      car.arrow = '<';
-    }
+      var intersection = Intersections.findOne({
+          $or: [
+              {"roads.ne.queue": carId}, 
+              {"roads.nw.queue": carId}, 
+              {"roads.sw.queue": carId}, 
+              {"roads.se.queue": carId}
+          ]
+      });
+      var id = intersection._id;
+      _.forEach(intersection.roads, function(val, key) {
+          console.log("key: ", key, "val: ", val)
+          for (var i = 0; i < val.queue.length; i++) {
+              if(val.queue[i] === carId) {
+                  //found the car in road!!!
+                  console.log("Found: ", val.queue[i], "in road: ", key);
+                  quadrant = key;
+                  car.current = quadrant;
+              }
+          };
+      });
+      if(car.current == 'nw' && car.direction == 'ne' || car.current == 'ne' && car.direction == 'se' || car.current == 'se' && car.direction == 'sw' || car.current == 'sw' && car.direction == 'nw'){
+        car.arrow = '<';
+      }
 
-    if(car.current == 'nw' && car.direction == 'sw' || car.current == 'sw' && car.direction == 'se' || car.current == 'se' && car.direction == 'ne' || car.current == 'ne' && car.direction == 'nw'){
-      car.arrow = '>';
-    }
+      if(car.current == 'nw' && car.direction == 'sw' || car.current == 'sw' && car.direction == 'se' || car.current == 'se' && car.direction == 'ne' || car.current == 'ne' && car.direction == 'nw'){
+        car.arrow = '>';
+      }
 
-    if(car.current == 'nw' && car.direction == 'se' || car.current == 'ne' && car.direction == 'sw' || car.current == 'se' && car.direction == 'nw' || car.current == 'sw' && car.direction == 'ne'){
-      car.arrow = '^';
-    }
+      if(car.current == 'nw' && car.direction == 'se' || car.current == 'ne' && car.direction == 'sw' || car.current == 'se' && car.direction == 'nw' || car.current == 'sw' && car.direction == 'ne'){
+        car.arrow = '^';
+      }
 
-    return car;
-
+      return car;
     }
 });
--- gridlock/client/menus/hud.js
+++ gridlock/client/menus/hud.js
@@ -1,3 +1,56 @@
 Template.hud.user = function () {
 	return Meteor.user();
 }
+
+Template.hud.nw = function () {
+  if (!!Meteor.user()) {
+    var roads =  Intersections.findOne({_id: Meteor.user().intersectionId}).roads;
+    // console.log("roads: ", roads);
+    var seMerger = null;
+    if (!!roads.nw.connectionId) {
+      seMerger = Intersections.findOne({_id: roads.nw.connectionId})
+      console.log("nw connection found");
+    }
+    // console.log("merger: ", seMerger);
+    return seMerger;
+  }  
+};
+Template.hud.ne = function () {
+  if (!!Meteor.user()) {
+    var roads =  Intersections.findOne({_id: Meteor.user().intersectionId}).roads;
+    // console.log("roads: ", roads);
+    var swMerger = null;
+    if (!!roads.ne.connectionId) {
+      swMerger = Intersections.findOne({_id: roads.ne.connectionId})
+      console.log("ne connection found")
+    }
+    // console.log("merger: ", swMerger);
+    return swMerger;
+  }  
+};
+Template.hud.sw = function () {
+  if (!!Meteor.user()) {
+    var roads =  Intersections.findOne({_id: Meteor.user().intersectionId}).roads;
+    // console.log("roads: ", roads);
+    var neMerger = null;
+    if (!!roads.sw.connectionId) {
+      neMerger = Intersections.findOne({_id: roads.sw.connectionId})
+      console.log("sw connection found")
+    }
+    // console.log("merger: ", neMerger);
+    return neMerger;
+  }  
+};
+Template.hud.se = function () {
+  if (!!Meteor.user()) {
+    var roads =  Intersections.findOne({_id: Meteor.user().intersectionId}).roads;
+    // console.log("roads: ", roads);
+    var nwMerger = null;
+    if (!!roads.se.connectionId) {
+      nwMerger = Intersections.findOne({_id: roads.se.connectionId})
+      console.log("se connection found")
+    }
+    // console.log("merger: ", nwMerger);
+    return nwMerger;
+  }  
+};
