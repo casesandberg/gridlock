a49d0a44b099caed5b19c510b1ac844e9747346f
Chris Gervang <gervangster@live.com>
2014-07-27 08:24:05 -0700

    car start pos

--- gridlock/client/car/car.js
+++ gridlock/client/car/car.js
@@ -1,6 +1,28 @@
 Template.car.rendered = function() {
-	//console.log(this.data);
-	var car = Cars.findOne(this.data);
-	console.log(car)
-	// $('#'+this.data).html(JSON.stringify(car))
+  //console.log(this.data);
+  var carId = this.data;
+  var quadrant;
+  var car = Cars.findOne(this.data);
+  console.log(car)
+  // $('#'+this.data).html(JSON.stringify(car))
+  var intersection = Intersections.findOne({
+    $or: [
+      {"roads.ne.queue": carId}, 
+      {"roads.nw.queue": carId}, 
+      {"roads.sw.queue": carId}, 
+      {"roads.se.queue": carId}
+    ]
+  });
+  var id = intersection._id;
+  _.forEach(intersection.roads, function(val, key) {
+    //console.log("key: ", key, "val: ", val)
+    for (var i = 0; i < val.queue.length; i++) {
+      if(val.queue[i] === carId) {
+        //found the car in road!!!
+        console.log("Found: ", val.queue[i], "in road: ", key);
+        quadrant = key;
+        console.log(quadrant);
+      }
+    };
+  });
 }
